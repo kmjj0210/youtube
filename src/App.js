6bf83b1e38871be35f779b84b9aef0a291af0b9b
minji0210 <kmjj0210@gmail.com>
2024-03-04 21:14:12 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-
+import { CiSearch } from "react-icons/ci";
 
 export default function App() {
+  const [text, setText] = useState('');
+  const handleChange = (e) => setText(e.target.value);
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    if (text === "") { 
+      return;
+    };
+
+    setText('');
+  }
   return (
     <>
+      <header className='p-2.5'>
+        <form
+          className='flex justify-center mx-auto items-center w-full'
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            placeholder='검색'
+            value={text}
+            onChange={handleChange}
+            className='focus:outline-none'
+          />
+          <button className='w-20'>
+            <CiSearch className='mx-auto' />
+          </button>
+        </form>
+      </header>
       <Outlet/>
     </>
   );
--- src/page/Videos.jsx
+++ src/page/Videos.jsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import { CiSearch } from "react-icons/ci";
+import React, { useState } from 'react';
 
 export default function Videos() {
   return (
-    <div className='container mx-auto'>
-      <header className='p-2.5'>
-        <form className='flex justify-center mx-auto items-center w-full max-w-200'>
-          <input
-            type="text"
-            placeholder='검색'
-            className='focus:outline-none'
-          />
-          <button className='w-20'>
-            <CiSearch />
-          </button>
-        </form>
-      </header>
+    <div>
+      
     </div>
   );
 }
