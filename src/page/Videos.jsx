ad3d239ce28fe1c32edebf14223436b1d5b937ba
minji0210 <kmjj0210@gmail.com>
2024-03-07 17:23:28 +0900

    no message

--- src/page/Videos.jsx
+++ src/page/Videos.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
+const getPopuplar = async () => {
+  // await new Promise((r) => setTimeout(r, 500))
+  let { data } = await axios.get('data/most_popuplar_videos.json')
+  return data.items
+}
+
+const getKeyword = async () => {
+  // await new Promise((r) => setTimeout(r, 500))
+  let { data } = await axios.get('data/list_by_keywords.json')
+  return data.items
+}
+
 export default function Videos() {
+  const queryClient = useQueryClient();
   const { keyword } = useParams();
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['video'],
-    queryFn: () =>
-      axios
-        .get('data/most_popuplar_videos.json')
-        .then((res) => res.data.items),
+
+  const popuplarQuery = useQuery({
+    queryKey: ['popuplar'],
+    queryFn: getPopuplar,
   })
 
-  if (isLoading) return 'Loading...'
-  if (error) return 'error... ' + error.message
+  const keywordQuery = useQuery({
+    queryKey: ['keyword'],
+    queryFn: getKeyword,
+  })
 
   return (
     <div className='mt-5'>
       Videos🔥
-      {data.map((a) => (
-        <p key={a.id}>{a.snippet.title}</p>
-      ))}
+      {
+        keyword ?
+          "있음" :
+          "없음"
+          // popuplarQuery.data.map((item) => (
+          //   <p key={item.id}>{item.snippet.title}</p>
+          // ))
+      }
     </div>
   );
 }
