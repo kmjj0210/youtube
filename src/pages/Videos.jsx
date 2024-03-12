14d21806aadf68ad184a3cb317aec991e8e94ebb
minji0210 <kmjj0210@gmail.com>
2024-03-12 20:33:41 +0900

    no message

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import VideoCard from '../componets/VideoCard';
-import Youtube from '../api/youtube';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../componets/VideoCard";
+import Youtube from "../api/youtube";
+import FakeYoutube from "../api/fakeYoutube";
 
 export default function Videos() {
   const { keyword } = useParams();
-  const { isLoading, error, data: videos } = useQuery({
-    queryKey: ['videos', keyword],
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ["videos", keyword],
     queryFn: () => {
       const youtube = new Youtube();
-      return youtube.search(keyword)
-    }
-  })
+      return youtube.search(keyword);
+    },
+  });
   return (
     <div className='py-5'>
-      <p className='font-bold text-2xl'>Videos {keyword ? `🔎${keyword}` : '🔥'}</p>
+      <p className='font-bold text-2xl'>
+        Videos {keyword ? `🔎${keyword}` : "🔥"}
+      </p>
       {isLoading && "loading..."}
       {error && "error..."}
       <ul>
-        {videos && (
-          videos.map(video => (
-            <VideoCard key={video.id} video={video} />
-          ))
-        )}
+        {videos &&
+          videos.map((video) => <VideoCard key={video.id} video={video} />)}
       </ul>
     </div>
   );
 }
-
